6ed11dd38101152a46c5d3902c8f2cf707a44971
EsquireRider <Esquirerider507@gmail.com>
2025-11-09 14:40:49 +0300

    Commit11

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -18,54 +19,40 @@ public class DeliveryCardTest {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    @BeforeEach
+    void setUp() {
+        Selenide.open("http://localhost:9999/");
+    }
+
+
     @Test
     void shouldValidFields() {
-        Selenide.open("http://localhost:9999/");
         SelenideElement form = $("form");
         form.$("[placeholder='Город']").setValue("Нижний Новгород");
         form.$("[placeholder='Дата встречи']").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
                 .setValue("20.11.2025");
         form.$("[name='name']").setValue("Тятяев Антон");
         form.$("[name='phone']").setValue("+79521112233");
-        $$("[data-test-id='agreement']").filter(Condition.visible).find(Condition.text("Я соглашаюсь"))
-                .click();
-        $$("button").filter(Condition.visible).find(Condition.text("Забронировать")).click();
-        $(Selectors.withText("Встреча успешно забронирована"))
-                .should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+        $("[data-test-id='notification']")
+                .should(Condition.text("Встреча успешно забронирована на 20.11.2025"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void shouldValidFieldsPlanDate() {
         String planningDate = generateDate(7, "dd.MM.yyyy");
-        Selenide.open("http://localhost:9999/");
         SelenideElement form = $("form");
         form.$("[placeholder='Город']").setValue("Нижний Новгород");
         form.$("[placeholder='Дата встречи']").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
                 .setValue(planningDate);
         form.$("[name='name']").setValue("Тятяев Антон");
         form.$("[name='phone']").setValue("+79521112233");
-        $$("[data-test-id='agreement']").filter(Condition.visible).find(Condition.text("Я соглашаюсь"))
-                .click();
-        $$("button").filter(Condition.visible).find(Condition.text("Забронировать")).click();
-        $(Selectors.withText("Встреча успешно забронирована"))
-                .should(Condition.visible, Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldValidFieldsDropDownList() {
-        Selenide.open("http://localhost:9999/");
-        SelenideElement form = $("form");
-        form.$("[placeholder='Город']").setValue("Ни");
-        $(Selectors.withText("Нижний Новгород")).click();
-        form.$("[placeholder='Дата встречи']").click();
-        $("[data-step='1']").click();
-        $("[data-day='1765141200000']").click();
-        form.$("[name='name']").setValue("Тятяев Антон");
-        form.$("[name='phone']").setValue("+79521112233");
-        $$("[data-test-id='agreement']").filter(Condition.visible).find(Condition.text("Я соглашаюсь"))
-                .click();
-        $$("button").filter(Condition.visible).find(Condition.text("Забронировать")).click();
-        $(Selectors.withText("Встреча успешно забронирована"))
-                .should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+        $("[data-test-id='notification']")
+                .should(Condition.text("Встреча успешно забронирована на 16.11.2025"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
